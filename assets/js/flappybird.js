39872408108548d9bc734db21fdbb160da946e48
Gregory <stary.grzegorz@gmail.com>
2025-01-28 07:15:18 +0000

    CREATED: Basic folder structure ADDED: Game images ADDED: style.css file ADDED: JavaScript file ADDED: First couple lines of code to create better game visualisation.

---
+++ assets/js/flappybird.js
@@ -0,0 +1,40 @@
+// board
+let board;
+let boardWidth = 360;
+let boardHeight = 640;
+let context;
+
+// bird
+let birdWidth = 34;
+let birdHeight = 24;
+let birdX = 50; // Adjusted to better starting position
+let birdY = 200;
+let birdImg = null;
+
+let bird = {
+    x: birdX,
+    y: birdY,
+    width: birdWidth,
+    height: birdHeight
+};
+
+window.onload = function () {
+    board = document.getElementById('board');
+    board.width = boardWidth;
+    board.height = boardHeight;
+    context = board.getContext("2d"); // Drawing on the board
+
+    // Drawing the bird (rectangle as fallback)
+    context.fillStyle = "green";
+    context.fillRect(bird.x, bird.y, bird.width, bird.height);
+
+    // Bird image
+    birdImg = new Image();
+    birdImg.src = "./assets/images/flappybird.png";
+
+    // Draw image after loading
+    birdImg.onload = function () {
+        context.clearRect(bird.x, bird.y, bird.width, bird.height); // Clear rectangle
+        context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+    };
+};
