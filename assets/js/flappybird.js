22dfe052440f68ade015df12bfa3367616289610
Gregory <stary.grzegorz@gmail.com>
2025-01-30 14:39:36 +0000

    ADDED: Draw the poles into the canvas.

--- assets/js/flappybird.js
+++ assets/js/flappybird.js
@@ -18,6 +18,16 @@ let bird = {
     height: birdHeight
 };
 
+// poles
+let poleArray = [];
+let poleWidth = 64;
+let poleHeight = 512;
+let poleX = boardWidth;
+let poleY = 0;
+
+let topPoleImg;
+let bottomPoleImg;
+
 window.onload = function () {
     board = document.getElementById('board');
     board.width = boardWidth;
@@ -37,7 +47,16 @@ window.onload = function () {
         context.clearRect(bird.x, bird.y, bird.width, bird.height); // Clear rectangle
         context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
     };
+
+    // Poles images load
+    topPoleImg = new Image();
+    topPoleImg.src = "./assets/images/toppipe.png";
+
+    bottomPoleImg = new Image();
+    bottomPoleImg.src = "./assets/images/bottompipe.png";
+
     requestAnimationFrame(update);
+    setInterval(placePole, 1500);
 };
 
 // For the game loop 
@@ -45,7 +64,26 @@ function update() {
     requestAnimationFrame(update);
     context.clearRect(0, 0, board.width, board.height);
 
-    // bird loop
+    // Bird loop
     context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+
+    // Pole loop
+    for (let i = 0; i < poleArray.length; i++) {
+        let pole = poleArray[i];
+        context.drawImage(pole.Image, pole.x, pole.y, pole.width, pole.height);
+    }
 }
 
+// Function for the poles
+
+function placePole() {
+    let topPole = {
+        img: topPoleImg,
+        x: poleX,
+        y: poleY,
+        width: poleWidth,
+        height: poleHeight,
+        passed: false
+    }
+    poleArray.push(topPole);
+}
